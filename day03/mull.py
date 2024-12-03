ebd76db9c77e56e3678e238e28b540a7e97a3956
Walt Mankowski <waltman@pobox.com>
2024-12-03 07:17:18 -0500

    added code to solve 3.2

--- day03/mull.py
+++ day03/mull.py
@@ -16,4 +16,18 @@ def main():
 
     print('Part 1:', part1)
 
+    part2 = 0
+    do = True
+    for line in lines:
+        toks = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", line)
+        for tok in toks:
+            if tok == "do()":
+                do = True
+            elif tok == "don't()":
+                do = False
+            elif do:
+                part2 += parse(tok)
+
+    print('Part 2:', part2)
+
 main()
