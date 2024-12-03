e52fe71be08afe14d2b676d895fdda1646f81d44
Walt Mankowski <waltman@pobox.com>
2024-12-03 07:07:13 -0500

    code for day 3 part 1

---
+++ day03/mull.py
@@ -0,0 +1,19 @@
+import sys
+import re
+
+def parse(tok):
+    m = re.match(r'mul\((\d{1,3}),(\d{1,3})\)', tok)
+    return int(m.group(1)) * int(m.group(2))
+
+def main():
+    with open(sys.argv[1]) as f:
+        lines = [line.rstrip() for line in f]
+
+    part1 = 0
+    for line in lines:
+        valids = re.findall(r'mul\(\d{1,3},\d{1,3}\)', line)
+        part1 += sum([parse(tok) for tok in valids])
+
+    print('Part 1:', part1)
+
+main()
