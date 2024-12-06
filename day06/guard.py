d38f611270521c50b36f30b0c06019513da8a1ac
Walt Mankowski <waltman@pobox.com>
2024-12-06 16:20:34 -0500

    Solution for day 6 part 2
    I just brute-forced the second part. I put an obstacle at every
    position the guard visited in the first part, then let him walk.
    Rather than try to detect loops, I decided if he hadn't exited after
    STEP_LIMIT steps then he was in a loop. I started with STEP_LIMIT=1000
    and kept increasing it by 1000 until the number didn't go down.
    
    Then I got tired of that and jumped from 5000 to 10000.
    
    It still only took 69 seconds, so not so bad.

--- day06/guard.py
+++ day06/guard.py
@@ -2,6 +2,32 @@ import sys
 import numpy as np
 from itertools import product
 
+def makes_loop(grid, position, start, deltas, next_dir):
+    nrows, ncols = grid.shape
+    STEP_LIMIT = 10000
+    grid[position[0], position[1]] = '#'
+    direction = 'up'
+    steps = 0
+    start_pos = position
+    
+    position = np.array(start)
+    steps = 0
+    while True:
+        if steps > STEP_LIMIT:
+            print(f'Loop, {start_pos=}, {steps=}')
+            return True
+        next_pos = position + deltas[direction]
+        if next_pos[0] < 0 or next_pos[0] >= nrows or next_pos[1] < 0 or next_pos[1] >= ncols:
+            # made it out, so no loop
+            print(f'No loop, {start_pos=}, {steps=}')
+            return False
+        elif grid[tuple(next_pos)] == '#':
+            # turn
+            direction = next_dir[direction]
+        else:
+            position = next_pos
+            steps += 1
+
 def main():
     # read in the grid
     with open(sys.argv[1]) as f:
@@ -32,6 +58,9 @@ def main():
 
     # now let the guard walk around
     seen = {tuple(position)}
+    start = tuple(position)
+    steps = 0
+    tour = [(int(position[0]), int(position[1]))]
     while True:
         next_pos = position + deltas[direction]
         if next_pos[0] < 0 or next_pos[0] >= nrows or next_pos[1] < 0 or next_pos[1] >= ncols:
@@ -43,7 +72,22 @@ def main():
         else:
             position = next_pos
             seen.add(tuple(position))
+            tour.append([int(x) for x in position])
+            steps += 1
 
     print('Part 1:', len(seen))
+    print('steps:', steps)
+    
+    part2 = 0
+    seen.remove(start)
+    i = 1
+    for position in seen:
+        print(f'{i=}')
+        i += 1
+        if makes_loop(grid.copy(), np.array(position), start, deltas, next_dir):
+            part2 += 1
+
+    print('Part 2:', part2)
+
 
 main()
