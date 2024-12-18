05aa1f55c2a8692352c52def4e99dff18d7a97f9
Walt Mankowski <waltman@pobox.com>
2024-12-18 12:11:30 -0500

    updated to solve day 18 part 2

--- day18/ram_run.py
+++ day18/ram_run.py
@@ -2,6 +2,16 @@ import sys
 import networkx as nx
 from itertools import product
 
+def neighbors(row, col, dim):
+    if row > 0:
+        yield (row-1, col)
+    if row < dim:
+        yield (row+1, col)
+    if col > 0:
+        yield (row, col-1)
+    if col < dim:
+        yield (row, col+1)
+
 def main():
     infile = sys.argv[1]
     dim, cutoff = (int(x) for x in sys.argv[2:4])
@@ -24,6 +34,14 @@ def main():
                 G.add_edge((row, col), (row+1, col))
 
     print('Part 1:', len(nx.shortest_path(G, (0,0), (dim, dim))) - 1)
-    
+
+    for i in range(cutoff, len(bytes)):
+        ebunch = tuple([(bytes[i], neighbor) for neighbor in neighbors(bytes[i][0], bytes[i][1], dim)])
+        G.remove_edges_from(ebunch)
+        try:
+            nx.shortest_path(G, (0,0), (dim, dim))
+        except nx.NetworkXNoPath:
+            print(f'Part 2: ({bytes[i][1]},{bytes[i][0]})')
+            break
 
 main()
