2dd3d88bdb8cfa86dda4a5d7a07d4380fe702656
Walt Mankowski <waltman@pobox.com>
2024-12-20 09:03:33 -0500

    wip attempting to figure out the algorithm

--- day17/chronospatial.py
+++ day17/chronospatial.py
@@ -77,4 +77,28 @@ def main():
     output = [str(val) for val in computer.run()]
     print('Part 1:', ",".join(output))
     
+#   begin = 35_184_372_000_000
+#    begin = 140_737_488_300_000
+    begin = 160_000_000_000_000
+    target = '2,4,1,1,7,5,1,5,4,3,5,5,0,3,3,0'
+    for i in range(begin, begin+1_000_000):
+        computer.A = i
+        computer.B = 0
+        computer.C = 0
+        res = [str(val) for val in computer.run()]
+        tst = ','.join(res)
+        print(i, len(res), tst)
+        if tst == target:
+            break
+
 main()
+
+# Program: 2,4,1,1,7,5,1,5,4,3,5,5,0,3,3,0
+# 2, 4: B = A % 8
+# 1, 1: B ^= 1
+# 7, 5: C = A / (2**B)
+# 1, 5: B ^= 5
+# 4, 3: B ^= C
+# 5, 5: output B % 8
+# 0, 3: A = A / 8
+# 3, 0: jump to 0
