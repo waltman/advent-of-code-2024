5051266b28d1c25fd506ee959a464681a9ecb445
Walt Mankowski <waltman@pobox.com>
2024-12-22 09:45:34 -0500

    code for day 21 part 1

--- day21/keypad.py
+++ day21/keypad.py
@@ -1,5 +1,11 @@
 import sys
 import networkx as nx
+from functools import cache
+
+numeric = nx.grid_graph(dim=(3, 4))
+numeric_pos = dict()
+directional = nx.grid_graph(dim=(3, 2))
+directional_pos = dict()
 
 def dirchar(from_pos, to_pos):
     char = {
@@ -12,24 +18,71 @@ def dirchar(from_pos, to_pos):
     delta = (from_pos[0]-to_pos[0], from_pos[1]-to_pos[1])
     return char[delta]
 
-def gen_seq(code, paths, pos):
-    sequence = []
-    cur = 'A'
-    for ch in code:
-        if ch != cur:
-            path = paths[cur][ch]
-            for i in range(len(path)-1):
-                sequence.append(dirchar(pos[path[i]], pos[path[i+1]]))
-        sequence.append('A')
-        cur = ch
-    return ''.join(sequence)
+@cache
+def gen_numeric_seqs(cur, code):
+    global numeric
+    global numeric_pos
+    
+    if len(code) == 1:
+        if code == cur:
+            return ['A']
+        else:
+            result = []
+            for path in nx.all_shortest_paths(numeric, cur, code):
+                seq = []
+                for i in range(len(path)-1):
+                    seq.append(dirchar(numeric_pos[path[i]], numeric_pos[path[i+1]]))
+                seq.append('A')
+                result.append(''.join(seq))
+            return result
+    else:
+        result = []
+        for path1 in nx.all_shortest_paths(numeric, cur, code[0]):
+            seq = []
+            for i in range(len(path1)-1):
+                seq.append(dirchar(numeric_pos[path1[i]], numeric_pos[path1[i+1]]))
+            seq.append('A')
+            seq1 = ''.join(seq)
+            for seq2 in gen_numeric_seqs(code[0], code[1:]):
+                result.append(seq1 + seq2)
+        return result
+
+@cache
+def gen_directional_seqs(cur, code):
+    global directional
+    global directional_pos
+    
+    if len(code) == 1:
+        if code == cur:
+            return ['A']
+        else:
+            result = []
+            for path in nx.all_shortest_paths(directional, cur, code):
+                seq = []
+                for i in range(len(path)-1):
+                    seq.append(dirchar(directional_pos[path[i]], directional_pos[path[i+1]]))
+                seq.append('A')
+                result.append(''.join(seq))
+            return result
+    else:
+        result = []
+        for path1 in nx.all_shortest_paths(directional, cur, code[0]):
+            seq = []
+            for i in range(len(path1)-1):
+                seq.append(dirchar(directional_pos[path1[i]], directional_pos[path1[i+1]]))
+            seq.append('A')
+            seq1 = ''.join(seq)
+            for seq2 in gen_directional_seqs(code[0], code[1:]):
+                result.append(seq1 + seq2)
+        return result
 
 def main():
     with open(sys.argv[1]) as f:
         codes = [line.rstrip() for line in f]
 
     # make a graph of the numeric keypad
-    numeric = nx.grid_graph(dim=(3, 4))
+    global numeric
+    global numeric_pos
     numeric.remove_node((3, 0))
     numeric_labels = {
         (0,0): '7',
@@ -45,11 +98,12 @@ def main():
         (3,2): 'A',
     }
     numeric = nx.relabel_nodes(numeric, numeric_labels)
-    numeric_paths = dict(nx.all_pairs_shortest_path(numeric))
     numeric_pos = {v:k for k,v in numeric_labels.items()}
 
     # make a graph of the numeric keypad
-    directional = nx.grid_graph(dim=(3, 2))
+    global directional
+    global directional_pos
+    
     directional.remove_node((0, 0))
     directional_labels = {
         (0,1): '^',
@@ -59,11 +113,18 @@ def main():
         (1,2): '>',
     }
     directional = nx.relabel_nodes(directional, directional_labels)
-    directional_paths = dict(nx.all_pairs_shortest_path(directional))
     directional_pos = {v:k for k,v in directional_labels.items()}
 
+    part1 = 0
     for code in codes:
-        sequence = gen_seq(gen_seq(gen_seq(code, numeric_paths, numeric_pos), directional_paths, directional_pos), directional_paths, directional_pos)
-        print(code, len(sequence))
+        minlen = 1e300
+        for path1 in gen_numeric_seqs('A', code):
+            for path2 in gen_directional_seqs('A', path1):
+                for path3 in gen_directional_seqs('A', path2):
+                    minlen = min(minlen, len(path3))
+        code_val = int(''.join([c for c in code if c.isnumeric()]))
+        part1 += code_val * minlen
+
+    print('Part 1:', part1)
 
 main()
